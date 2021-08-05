c18d26860dd8de46ffbad6249406b9b03a162da2
aaaRooshen <aaa.rooshen@gmail.com>
2021-08-06 02:05:21 +0300

    hwKotlinLesson8 las v.1

---
+++ app/src/main/java/qr/a/rooshen/hwkotlinlesson8/MainActivity.kt
@@ -0,0 +1,57 @@
+package qr.a.rooshen.hwkotlinlesson8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity(), MainNavigation {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        openAuthorizationFragment()
+
+    }
+
+    override fun openMenu() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.clRootActivity, MenuFragment())
+            .commit()
+    }
+
+    override fun openMenuList() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.clRootActivity, MenuListFragment())
+            .commit()
+    }
+
+    override fun openMenuListContainerMenu() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.flContainerMenu, MenuListFragment())
+            .commit()
+    }
+
+    override fun openDialogBasket() {
+        DialogBasket().show(supportFragmentManager, "BottomTest")
+    }
+
+    override fun openAuthorizationFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.clRootActivity, AuthorizationFragment())
+            .commit()
+    }
+
+    override fun openBasketFragment(phoneNumber: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.clRootActivity, BasketFragment.newInstance(phoneNumber))
+            .addToBackStack("BasketFragment")
+            .commit()
+    }
+
+}
---
+++ app/src/main/java/qr/a/rooshen/hwkotlinlesson8/MenuListFragment.kt
@@ -0,0 +1,43 @@
+package qr.a.rooshen.hwkotlinlesson8
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import qr.a.rooshen.hwkotlinlesson8.databinding.FragmentMenuListBinding
+
+
+class MenuListFragment() : Fragment() {
+
+    private lateinit var binding: FragmentMenuListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMenuListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val vpMain = binding.vpMainMenuList
+        vpMain.adapter = ViewPagerAdapter(this).apply {
+            addFragment(AllMenuFragment())
+            addFragment(FastFoodFragment())
+            addFragment(DessertsFragment())
+        }
+
+        val tabNames = listOf("Все", "Фаст-фуд", "Десерты")
+        vpMain.isUserInputEnabled = false
+        val tbMain = binding.tbMainMenuList
+        TabLayoutMediator(tbMain, vpMain) { tab, position ->
+            tab.text = tabNames[position]
+        }.attach()
+
+    }
+}
